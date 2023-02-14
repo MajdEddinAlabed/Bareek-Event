2e0aba9f14965171f93a1516e367cc0523c0b090
Nostolgic <ayyad.khalid9999@gmail.com>
2023-02-14 16:49:25 +0200

    gg

--- lib/main.dart
+++ lib/main.dart
@@ -15,13 +15,6 @@ void main() {
   runApp(const MyApp());
 }
 
-// mjjd sho bdk?
-//smd noob
-//brr
-// test2
-//nosto is gayyyyyy 
-//kol 5raaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
