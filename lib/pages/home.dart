50dd386ab4eaff708945cab63e134644fa062f5e
Nostolgic <ayyad.khalid9999@gmail.com>
2023-02-15 18:35:34 +0200

    Fullscreen Carousel & Gesture Drawer

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -4,59 +4,91 @@ import 'package:flutter/material.dart';
 
 import '../utils/drawer.dart';
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
   static String name = '/home_screen';
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final GlobalKey<ScaffoldState> _scaffoldkey = new GlobalKey<ScaffoldState>();
+  bool _isFABVisible = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
+      key: _scaffoldkey,
       drawer: DrawerClass(),
-      appBar: AppBar(),
-      body: Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-        Text(
-          'Featured Events',
-          style: TextStyle(
-              fontSize: 32,
-              fontStyle: FontStyle.italic,
-              foreground: Paint()
-                ..style = PaintingStyle.stroke
-                ..strokeWidth = 2.5
-                ..color = Colors.deepPurple),
-        ),
-        CarouselSlider(
-            options: CarouselOptions(
-              height: 600,
-              autoPlay: false,
-              enlargeCenterPage: true,
-              viewportFraction: 0.8,
-            ),
-            items: [
-              Card(
-                elevation: 10,
-                color: Colors.black,
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: imageSliders,
+      body: GestureDetector(
+          onHorizontalDragUpdate: (details) {
+            // Show FAB when user swipes up and hide FAB when user swipes down
+            setState(() {
+              _isFABVisible = details.delta.dy > 0;
+            });
+          },
+          child: Stack(
+            children: [
+              ListView(
+                children: [
+                  Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        CarouselSlider(
+                            options: CarouselOptions(
+                              height: MediaQuery.of(context).size.height,
+                              viewportFraction: 1,
+                            ),
+                            items: [
+                              Card(
+                                elevation: 10,
+                                color: Colors.black,
+                                child: SingleChildScrollView(
+                                  child: Column(
+                                    children: imageSliders,
+                                  ),
+                                ),
+                              ),
+                            ]),
+                      ]),
+                ],
+              ),
+              Positioned(
+                bottom: 5.0,
+                right: 16.0,
+                child: AnimatedOpacity(
+                  opacity: _isFABVisible ? 1.0 : 0.0,
+                  duration: Duration(milliseconds: 200),
+                  child: FloatingActionButton(
+                    onPressed: () => _scaffoldkey.currentState!.openDrawer(),
+                    child: Icon(Icons.add),
                   ),
                 ),
               ),
-            ]),
-      ]),
+            ],
+          )),
     );
   }
 }
 
 final List<Widget> imageSliders = imgList
     .map((item) => Container(
-          height: 300,
+          height: 220,
+          width: double.infinity,
           color: Colors.black,
           margin: EdgeInsets.all(5.0),
           child: ClipRRect(
               borderRadius: BorderRadius.all(Radius.circular(5.0)),
               child: Stack(
                 children: <Widget>[
-                  Image.network(item, fit: BoxFit.cover, width: 1000.0),
+                  Image.network(
+                    item,
+                    fit: BoxFit.fill,
+                    width: double.infinity,
+                    height: double.infinity,
+                  ),
                   Positioned(
                     bottom: 0.0,
                     left: 0.0,
@@ -65,7 +97,7 @@ final List<Widget> imageSliders = imgList
                       decoration: BoxDecoration(
                         gradient: LinearGradient(
                           colors: const [
-                            Color.fromARGB(200, 0, 0, 0),
+                            Color.fromARGB(0, 0, 0, 0),
                             Color.fromARGB(0, 0, 0, 0)
                           ],
                           begin: Alignment.bottomCenter,
@@ -95,5 +127,8 @@ final List<String> imgList = [
   'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
   'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
   'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
-  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80',
+  'https://media-cdn.tripadvisor.com/media/photo-s/03/9b/2f/5b/cairo.jpg',
+  'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+  'https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg',
 ];
