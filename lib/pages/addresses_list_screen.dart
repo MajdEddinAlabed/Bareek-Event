b6ffea9c7301bd2e2340e6b20b516c264c01408a
Majd Eddin Alabed <majdeddin6@gmail.com>
2023-02-28 18:30:07 +0200

    fixing design adding a popup

--- lib/pages/addresses_list_screen.dart
+++ lib/pages/addresses_list_screen.dart
@@ -12,8 +12,8 @@ class _AddressesListScreenState extends State<AddressesListScreen> {
   String pageTitle = 'My Addresses';
   int count = 3;
   bool showCheckboxes = false;
-  List<bool> checked = List.filled(3, false);
   List<bool> isSelected = [];
+  int? editingCardIndex = -1;
 
   @override
   void initState() {
@@ -40,107 +40,122 @@ class _AddressesListScreenState extends State<AddressesListScreen> {
     });
   }
 
-  Widget _buildDeleteButton() {
-    if (isSelected.contains(true)) {
-      return IconButton(
-        icon: const Icon(Icons.delete),
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return AlertDialog(
-                title: const Text('Delete selected addresses?'),
-                actions: [
-                  TextButton(
-                    onPressed: () {
-                      setState(() {
-                        deleteSelected();
-                      });
-                      Navigator.pop(context);
-                    },
-                    child: const Text('Delete'),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      setState(() {
-                        isSelected = List<bool>.generate(count, (int index) => false);
-                      });
-                      Navigator.pop(context);
-                    },
-                    child: const Text('Cancel'),
-                  ),
-                ],
-              );
-            },
-          );
-        },
-      );
-    } else {
-      return const IconButton(
-        icon: Icon(Icons.delete_outline),
-        onPressed: null,
-      );
-    }
-  }
-
-    @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(pageTitle),
         actions: [
-          _buildDeleteButton(),
+          if (showCheckboxes || editingCardIndex != null)
+            IconButton(
+              icon: const Icon(Icons.clear),
+              onPressed: () {
+                setState(() {
+                  showCheckboxes = false;
+                  editingCardIndex = null;
+                  isSelected = List<bool>.generate(count, (int index) => false);
+                });
+              },
+            ),
           IconButton(
-            icon: const Icon(Icons.add),
-            onPressed: addAddress,
+            icon: const Icon(Icons.more_vert),
+            onPressed: () {
+              setState(() {
+                showCheckboxes = !showCheckboxes;
+              });
+            },
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: count,
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            color: Colors.blue,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        DefaultTextStyle(
-                          style: const TextStyle(color: Colors.white),
-                          child: Text(
-                            'Address ${index + 1}',
-                            style: const TextStyle(fontSize: 18.0),
-                          ),
-                        ),
-                        const SizedBox(height: 8.0),
-                        const DefaultTextStyle(
-                          style: TextStyle(color: Colors.white),
-                          child: Text(
-                            '123 Main St\nAnytown, USA 12345',
-                            style: TextStyle(fontSize: 16.0),
-                          ),
-                        ),
-                      ],
-                    ),
+      body: Stack(
+        children: [
+          addressesCard(),
+          Positioned(
+            bottom: 16,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 56,
+              margin: const EdgeInsets.symmetric(horizontal: 16),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(28),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    spreadRadius: 1,
+                    blurRadius: 5,
+                    offset: const Offset(0, 3),
                   ),
-                  Checkbox(
-                    value: isSelected[index],
-                    onChanged: (value) {
-                      setState(() {
-                        isSelected[index] = value!;
-                      });
+                ],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: isSelected.contains(true)
+                        ? () {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title:
+                                      const Text('Delete selected addresses?'),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          deleteSelected();
+                                        });
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text('Delete'),
+                                    ),
+                                    TextButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          isSelected = List<bool>.generate(
+                                              count, (int index) => false);
+                                        });
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text('Cancel'),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          }
+                        : null,
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.add),
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: const Text('Popup Title'),
+                            content: const Text('This is my popup.'),
+                            actions: [
+                              TextButton(
+                                child: const Text('Close'),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
                     },
                   ),
                 ],
               ),
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
@@ -150,45 +165,39 @@ class _AddressesListScreenState extends State<AddressesListScreen> {
       itemCount: count,
       itemBuilder: (BuildContext context, int index) {
         return Card(
-          color: Colors.blue,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      DefaultTextStyle(
-                        style: const TextStyle(color: Colors.white),
-                        child: Text(
-                          'Address ${index + 1}',
-                          style: const TextStyle(fontSize: 18.0),
-                        ),
-                      ),
-                      const SizedBox(height: 8.0),
-                      const DefaultTextStyle(
-                        style: TextStyle(color: Colors.white),
-                        child: Text(
-                          '123 Main St\nAnytown, USA 12345',
-                          style: TextStyle(fontSize: 16.0),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                if (showCheckboxes)
-                  Checkbox(
+          elevation: 5,
+          shadowColor: Colors.grey.withOpacity(0.5),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+            side: BorderSide(
+              color: Colors.grey.withOpacity(0.2),
+              width: 1,
+            ),
+          ),
+          child: ListTile(
+            leading: showCheckboxes
+                ? Checkbox(
                     value: isSelected[index],
-                    onChanged: (value) {
+                    onChanged: (bool? value) {
                       setState(() {
                         isSelected[index] = value!;
                       });
                     },
-                  ),
-              ],
-            ),
+                  )
+                : null,
+            title: const Text('Address Name'),
+            subtitle: const Text('1234 Address St.'),
+            trailing: showCheckboxes
+                ? IconButton(
+                    icon: const Icon(Icons.edit),
+                    onPressed: () {
+                      setState(() {
+                        editingCardIndex =
+                            index; // set editingCardIndex to the index of the card being edited
+                      });
+                    },
+                  )
+                : null,
           ),
         );
       },
